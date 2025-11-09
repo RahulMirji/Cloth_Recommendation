3ae85c0bca0bcda74c30502b67bb329f3c05525b
Rahul Mirji <rahulmirji444@gmail.com>
2025-11-09 10:52:06 +0530

    pi api is failing

--- VirtualTryOn/constants/index.ts
+++ VirtualTryOn/constants/index.ts
@@ -5,7 +5,7 @@
  */
 
 export const PI_API_CONFIG = {
-  API_KEY: '247423cdf4ab3e0ecf6937fb1c3d1424cb625bb53f2cce9ad0b697eca7308dc8',
+  API_KEY: '67f42c645da7987e73ad4f8b46fff721d9bd53c9bc36b7f759182bf4514ad2de',
   ENDPOINT: 'https://api.piapi.ai/api/v1/task',
   MODEL: 'gemini',
   TASK_TYPE: 'gemini-2.5-flash-image',
